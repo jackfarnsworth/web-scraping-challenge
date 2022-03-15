8bef90d301cc2a6fcaa6ff3e3264da6297a6880f
jackcfarnsworth <jackcfarnsworth@gmail.com>
2022-03-15 18:53:04 -0400

    first final draft

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+
+@app.route("/")
+def index():
+    data = mongo.db.data.find_one()
+    return render_template("index.html", data=data)
+
+@app.route("/scrape")
+def scraper():
+    data = mongo.db.data
+    mars_data = scrape_mars.scrape()
+    data.update_one({}, {"$set": mars_data}, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import pymongo
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    with Browser('chrome', **executable_path, headless=False) as browser:
+        #get news items
+        browser.visit('https://redplanetscience.com/')
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+
+        news_title = soup.find('div', class_="content_title").text
+        news_p = soup.find('div', class_="article_teaser_body").text
+
+        #get featured image
+        browser.visit('https://spaceimages-mars.com/')
+        browser.links.find_by_partial_text('FULL IMAGE').click()
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        featured_image_url = f'https://spaceimages-mars.com/{soup.find("img", class_="headerimage")["src"]}'
+
+        #get table
+        tables = pd.read_html('https://galaxyfacts-mars.com/')
+        mars_df = tables[1]
+        mars_df.set_index(0, inplace=True)
+
+        table = mars_df.to_html()
+        table.replace('\n', '')
+
+        #get hemisphere images
+        url = 'https://marshemispheres.com/'
+        browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+
+        links = soup.find_all('div', class_='item')
+
+        hemisphere_image_urls = []
+        for link in links:
+            browser.visit(url + link.find("a")["href"])
+            html = browser.html
+            soup = BeautifulSoup(html, 'html.parser')
+            hemisphere_image_urls.append({"title" : link.find('h3').text.replace(" Enhanced", ''), 
+                                        "img_url": url + soup.find('div', class_='downloads').find('a')['href']})
+    return {"newstitle" : news_title,
+            "newsparagraph": news_p,
+            "featuredimage": featured_image_url,
+            "table": table,
+            "hemispheres": hemisphere_image_urls}
+
+
